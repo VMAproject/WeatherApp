c835978f6d1805836703535fb59a7a4a144f023b
Verlamov Michail <verlamov.m@gmail.com>
2016-12-22 17:43:35 +0200

    Translation from XML to Java classes

---
+++ src/main/java/com/weather/config/Initializer.java
@@ -0,0 +1,40 @@
+package com.weather.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * @Author Verlamov Michail
+ * @Mail verlamov.m@gmail.com
+ * @GitHub VMAproject
+ * Date 22.12.16 Time 17:12
+ */
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    protected Filter[] getServletFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return new Filter[]{encodingFilter};
+    }
+}
